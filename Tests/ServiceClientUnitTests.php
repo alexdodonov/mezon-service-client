6f058f1ccb943cc265e51bf625628f21f7f7e374
gdever <alexey@dodonov.pro>
2020-05-29 10:36:23 +0300

    bugfix/Build error was fixed

--- Tests/ServiceClientUnitTests.php
+++ Tests/ServiceClientUnitTests.php
@@ -94,7 +94,7 @@ class ServiceClientUnitTests extends \PHPUnit\Framework\TestCase
         ]): object
     {
         return $this->getMockBuilder($this->clientClassName)
-            ->onlyMethods($methods)
+            ->setMethods($methods)
             ->disableOriginalConstructor()
             ->getMock();
     }
